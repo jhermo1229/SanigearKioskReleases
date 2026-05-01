3d0d3c2c1979ea6b83399bbe54e012c4b65b6df3
jhermo <jbhermo@yahoo.com>
2026-05-01 10:43:01 -0400

    Fix Wifi access

--- app/src/main/java/com/sanigear/kioskapp/MainActivity.java
+++ app/src/main/java/com/sanigear/kioskapp/MainActivity.java
@@ -317,17 +317,11 @@ public class MainActivity extends Activity {
 
     private void openWifiSettings() {
         try {
-            Intent intent;
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                intent = new Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY);
-            } else {
-                intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
-            }
-
+            Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         } catch (Exception e) {
-            Toast.makeText(this, "Cannot open Wi-Fi", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Cannot open Wi-Fi settings", Toast.LENGTH_SHORT).show();
         }
     }
 
